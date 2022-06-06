a1e54a0712978a9964162a91101f8eb74aee4280
Pritam Dash <pritamdash2015@outlook.com>
2022-06-06 23:44:05 +0530

    Addition of a new class and UI of screen complete

---
+++ MultiScreenApp/VoterViewController.swift
@@ -0,0 +1,12 @@
+//
+//  VoterViewController.swift
+//  MultiScreenApp
+//
+//  Created by Pritam Dash on 06/06/22.
+//
+
+import Foundation
+import UIKit
+class VoterViewController : UIViewController {
+    
+}
