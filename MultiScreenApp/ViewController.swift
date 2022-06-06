c16cbaf5f8e94514e1162976707d826dd572e86c
Pritam Dash <pritamdash2015@outlook.com>
2022-06-06 22:39:15 +0530

    Initial Commit

---
+++ MultiScreenApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MultiScreenApp
+//
+//  Created by Pritam Dash on 06/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
